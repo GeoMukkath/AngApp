49aee87f9736c909ea3c611b29c8478e6e26196f
geomukkath <geomukkath@yandex.com>
2021-02-13 20:27:08 +0530

    added module called company

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { CompanyModule } from './company/company.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { EmployeeComponent } from './employee/employee.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CompanyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeeLogInComponent } from './employee-log-in/employee-log-in.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EmployeeLogInComponent],
   imports: [
     CommonModule
   ]
